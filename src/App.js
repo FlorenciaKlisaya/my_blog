1ce29facda710bf4347909acca1b62b28237ea7e
FlorenciaKlisaya <calisayaflor3@gmail.com>
2025-10-05 22:08:23 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ContactoPage from "./components/contacto/ContactoPage";
+
+function App() {
+  return (
+    <div>
+      <ContactoPage />
+    </div>
+  );
+}
+
+export default App;
